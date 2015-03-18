ca4872ae2f37fde04d084eed377a21fd1b8d3201
Sönke Hahn <soenke.hahn@zalora.com>
2015-03-18 17:04:13 +0800

    wip

--- src/Example.hs
+++ src/Example.hs
@@ -5,16 +5,18 @@
 {-# LANGUAGE TypeSynonymInstances #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE DataKinds #-}
 
 module Main where
 
 import Generics.SOP
+import Safe
 import qualified GHC.Generics as GHC
 import Options.Applicative
 
 main :: IO ()
 main = withOption $ \option ->
-  print $ port option
+  print (option :: Option)
 
 withOption :: (Generic a, HasDatatypeInfo a, All2 ToOption (Code a), All Show (Map ConstructorInfo (Code a))) => (a -> IO ()) -> IO ()
 withOption action = execParser opts >>= action
@@ -24,24 +26,28 @@ withOption action = execParser opts >>= action
 parser :: forall a. (Generic a, HasDatatypeInfo a, All2 ToOption (Code a), All Show (Map ConstructorInfo (Code a)))
        => Parser a
 parser = case datatypeInfo (Proxy :: Proxy a) of
-           ADT _ _ cs -> parser' cs
-           Newtype _ _ c -> parser' (c :* Nil)
+           ADT _ _ cs -> to <$> SOP <$> parser' cs
+           -- Newtype _ _ c -> parser' (c :* Nil)
   where
-    parser' :: NP ConstructorInfo (Code a) -> Parser a
-    parser' (Record _ fields :* Nil) = parser'' fields
-    parser' cs = error $ ""
+    parser' ::
+      NP ConstructorInfo (Code a) -> Parser (NS (NP I) (Code a))
+    parser' (Record _ fields :* Nil) = Z <$> parser'' fields
+    parser' _cs = error $ ""
 
-    parser'' :: NP FieldInfo xs -> Parser a
-    parser'' fields = error $ show ""
+    parser'' :: (All ToOption xs) => NP FieldInfo xs -> Parser (NP I xs)
+    parser'' Nil = pure Nil
+    parser'' (field :* r) =
+      (:*) <$> (I <$> goField field) <*> (parser'' r)
       --to . SOP . Z <$> hsequence (hcpure (Proxy :: Proxy ToOption) (toOpt "abc"))
 
-goField :: (ToOption a) => FieldInfo a -> K (Parser String) a
-goField (FieldInfo field) = K $ toOpt field
+goField :: (ToOption a) => FieldInfo a -> Parser a
+goField (FieldInfo field) = toOpt field
 
 data Option = Option
   { port :: String
   , debug :: Bool
-  } deriving (GHC.Generic)
+  , int :: Int
+  } deriving (GHC.Generic, Show)
 
 instance Generic Option
 instance HasDatatypeInfo Option
@@ -54,3 +60,7 @@ instance ToOption String where
 
 instance ToOption Bool where
   toOpt name = switch (long name)
+
+instance ToOption Int where
+  toOpt name =
+    readMay <$> strOption (long name)
