dccfd5678562040f2f586810ae3b4bed48cb957a
Sönke Hahn <soenke.hahn@zalora.com>
2015-03-19 12:02:26 +0800

    add test cases for invalid arguments

--- test/System/Console/Args/GenericsSpec.hs
+++ test/System/Console/Args/GenericsSpec.hs
@@ -1,10 +1,15 @@
-{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveGeneric       #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module System.Console.Args.GenericsSpec where
 
+import           Control.Exception
 import           Generics.SOP
 import qualified GHC.Generics
 import           System.Environment
+import           System.Exit
+import           System.IO
+import           System.IO.Silently
 import           Test.Hspec
 
 import           System.Console.Args.Generics
@@ -19,6 +24,21 @@ spec = do
     it "allows optional arguments" $ do
       withArgs (words "--baz foo") $ withArguments $ \ options ->
         options `shouldBe` Foo Nothing "foo" False
+
+    context "invalid arguments" $ do
+      it "doesn't execute the action" $ do
+        let main = withArgs (words "--invalid") $
+              withArguments $ \ (_ :: Foo) ->
+                throwIO (ErrorCall "action")
+        main `shouldThrow` (== ExitFailure 1)
+
+      it "prints out an error" $ do
+        output <- hCapture_ [stderr] $ handle (\ (_ :: SomeException) -> return ()) $
+          withArgs (words "--no-such-option") $
+            withArguments $ \ (_ :: Foo) -> return ()
+        output `shouldContain` "Invalid"
+        output `shouldContain` "--no-such-option"
+
 
 data Foo
   = Foo {
