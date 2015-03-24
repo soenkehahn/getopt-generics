42bd8601cffc10cfced3a2e2d05135276bdfd625
kenny shen <kenny@machinesung.com>
2015-03-24 14:58:12 +0800

    add example usage

---
+++ example/example.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE DeriveGeneric #-}
+
+import           Generics.SOP
+import qualified GHC.Generics
+import           System.Console.Args.Generics
+import           System.Environment
+
+-- we specify a data type that maps to the commandline arguments
+
+data Foo
+  = Foo {
+      bar :: String
+    , baz :: Maybe String
+    , qux :: Int
+    }
+    deriving (Eq, Show, GHC.Generics.Generic)
+
+instance Generic Foo
+instance HasDatatypeInfo Foo
+
+-- give it go
+
+main :: IO ()
+main = do
+  args <- getArgs
+  withArgs args $ withArguments $ \ myFoo -> do
+    print $ bar myFoo
+    print $ baz myFoo
+    print $ qux myFoo
